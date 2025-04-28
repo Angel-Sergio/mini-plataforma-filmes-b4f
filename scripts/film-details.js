29fc50c02052a332382a16c6f22c34ef93d3ad67
Angel-Sergio <sergioangel947@gmail.com>
2025-04-28 16:39:41 +0200

    feat: creat a function  to show film's details

--- scripts/film-details.js
+++ scripts/film-details.js
@@ -1,1 +1,16 @@
 // Função para mostrar detalhes de um filme
+
+import { films } from "../data/db.js";
+function newfilms() {
+  document.addEventListener("DOMContentLoaded", () => {
+    const idInput = document.getElementById("id");
+    const titleInput = document.getElementById("title");
+    const genderInput = document.getElementById("gender");
+    const yearInput = document.getElementById("year");
+    const rateInput = document.getElementById("rate");
+    const descriptionInput = document.getElementById("description");
+    const imageUrlInput = document.getElementById("imageUrl");
+  });
+}
+
+console.log(newfilms);
